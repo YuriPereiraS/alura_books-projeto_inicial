0d37b2c2444c255af5a9e9de659a3d248f161f88
Yuri Soares <113906391+YuriPereiraS@users.noreply.github.com>
2023-10-09 18:32:42 -0300

    Delete alura_books-projeto_inicial directory

--- alura_books-projeto_inicial/app/metodoMap.js
+++
@@ -1,7 +0,0 @@
-function aplicarDescontos(livros) {
-    const desconto = 0.3
-    livrosComDesconto = livros.map(livro => {
-        return {...livro, preco: livro.preco - (livro.preco * desconto)}
-    })
-    return livrosComDesconto
-}
--- alura_books-projeto_inicial/app/metodoSort.js
+++
@@ -1,7 +0,0 @@
-let btnOdernarPorPreco = document.getElementById('btnOrdenarPorPreco')
-btnOdernarPorPreco.addEventListener('click', ordenarLivrosPreço)
-
-function ordenarLivrosPreço() {
-    let livrosOrdenados = livros.sort((a, b) => a.preco - b.preco) 
-    exibirLivrosNaTela(livrosOrdenados)
-}
